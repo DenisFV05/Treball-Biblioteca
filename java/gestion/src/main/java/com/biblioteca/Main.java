0564704103a8cbc8178f02d9cfe495c62e3e9909
Bermu <alexbermudezcarmona@gmail.com>
2025-01-10 17:39:26 +0100

    .

--- java/gestion/src/main/java/com/biblioteca/Main.java
+++ java/gestion/src/main/java/com/biblioteca/Main.java
@@ -60,4 +60,79 @@ public class Main {
         do {
             System.out.println("Gestió de llibres\n1. Afegir" +
                               "\n2. Modificar" +
-                  
+                              "\n3. Eliminar" +
+                              "\n4. Llistar" +
+                              "\n0. Tornar al menú principal" +
+                              "\nEscull una opció: ");
+            if (scanner.hasNextInt()) {
+                optLibros = scanner.nextInt();
+
+                switch (optLibros) {
+                    case 1:
+                        // Afegir llibre
+                        break;
+                    case 2:
+                        // Modificar llibre
+                        break;
+                    case 3:
+                        // Eliminar llibre
+                        break;
+                    case 4:
+                        limpiarPantalla();
+                        menuListarLibros(scanner);
+                        break;
+                    case 0:
+                        limpiarPantalla();
+                        break;
+                    default:
+                        System.out.println("Opció no vàlida. Torna-ho a intentar.\n");
+                }
+            } else {
+                System.out.println("Si us plau, ingressa un número vàlid.\n");
+                scanner.next();
+                optLibros = -1;
+            }
+        } while (optLibros != 0);
+    }
+
+    // Menú per llistar llibres
+    public static void menuListarLibros(Scanner scanner) {
+        int optListarLibros;
+
+        do {
+            System.out.println("Llistar llibres\n1. Tots" +
+                                "\n2. En préstec" +
+                                "\n3. Per autor" +
+                                "\n4. Cercar títol" +
+                                "\n0. Tornar al menú principal" +
+                                "\nEscull una opció: ");
+            if (scanner.hasNextInt()) {
+                optListarLibros = scanner.nextInt();
+
+                switch (optListarLibros) {
+                    case 1:
+                        // Llistar tots els llibres
+                        break;
+                    case 2:
+                        // Llistar llibres en préstec
+                        break;
+                    case 3:
+                        // Llistar per autor
+                        break;
+                    case 4:
+                        // Cercar títol
+                        break;
+                    case 0:
+                        limpiarPantalla();
+                        break;
+                    default:
+                        System.out.println("Opció no vàlida. Torna-ho a intentar.\n");
+                }
+            } else {
+                System.out.println("Si us plau, ingressa un número vàlid.\n");
+                scanner.next();
+                optListarLibros = -1;
+            }
+        } while (optListarLibros != 0);
+    }
+}
