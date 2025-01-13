6d5f761261b9dc6118445c2175a0b60c9a5894c5
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2025-01-13 17:52:18 +0100

    .

--- src/main/java/com/biblioteca/Usuari.java
+++ src/main/java/com/biblioteca/Usuari.java
@@ -1,11 +1,6 @@
 package com.biblioteca;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Scanner;
+
 
 public class Usuari {
     
