df728b80f1aaf5feb17a67cbba558a8fa6563587
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2025-01-13 16:40:10 +0100

    Creación de clases
    En estas clases la idea es poner las funciones para interactuar con los json

---
+++ src/main/java/com/biblioteca/Llibre.java
@@ -0,0 +1,15 @@
+package com.biblioteca;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Llibre {
+
+    private static final String FILE_PATH = "./llibres.json";
+
+    
+}
