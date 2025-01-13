8561c46c646310d52354e6ca40a89f0425fed0d7
Javier-lopez-mestre <Xalomes276@gmail.com>
2025-01-13 20:44:47 +0100

    Llistat de llibres per autor

--- src/main/java/com/biblioteca/Main.java
+++ src/main/java/com/biblioteca/Main.java
@@ -4,6 +4,7 @@ package com.biblioteca;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -11,10 +12,16 @@ import org.json.JSONObject;
 public class Main {
     public static void main(String[] args) {
         try{
+            
             // Ruta de l'arxiu json llibres
             String rutaJsonLlibres = "src/main/json/llibres.json";
             String contingut = new String(Files.readAllBytes(Paths.get(rutaJsonLlibres)));
-
+            
+            // Demanem la paraula autor
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Introduce el nombre del autor: ");
+            String autorIntroduit = scanner.nextLine();
+            
             // Convertim el contingut a un JSONArray
             JSONArray llibres = new JSONArray(contingut);
 
@@ -27,27 +34,33 @@ public class Main {
                 int id = llibre.getInt("id");
                 String titol = llibre.getString("titol");
                 JSONArray autor = llibre.getJSONArray("autor");
-
-                // Mostrem les dades del llibre
+                
+                // Mostrem la capçelera de la llista
                 if (i == 0){
-                    System.out.println("-".repeat(38) + "   " + "LLISTAT DE LLIBRES" + "    " + "-".repeat(38));
+                    System.out.println("-".repeat(33) + "   " + "LLISTAT DE LLIBRES PER AUTOR" + "    " + "-".repeat(33));
                     System.out.printf("%-5s | %-45s | %-35s", "ID", "TITOL", "AUTOR");
                     System.out.println();
                     System.out.println("-".repeat(100));
                 }
-                System.out.printf("%-5d | %-45s | ", id, titol);
+
+                // Si l'autor introduit es troba, es mostren les dades d'aquest
                 for (int j = 0; j < autor.length(); j++){
-                    if (j != 0){
-                        System.out.print(" / " + autor.getString(j));
-                    }else{
-                        System.out.print(autor.getString(j));
+                    if (autorIntroduit.equals(autor.getString(j))){
+                        System.out.printf("%-5s | %-45s | ", id, titol);
+                        for (int k = 0; k < autor.length(); k++){
+                            if (k != 0){
+                                System.out.print(" / " + autor.getString(j));
+                            }else{
+                                System.out.print(autor.getString(j));
+                            }
+                        }
+                        System.out.println("");
+                        if (i == (llibres.length() - 1)){
+                            System.out.println();
+                        } else {
+                            System.out.println("-".repeat(100));
+                        }
                     }
-                }
-                System.out.println("");
-                if (i == (llibres.length() - 1)){
-                    System.out.println();
-                } else {
-                    System.out.println("-".repeat(100));
                 }
             }
         } catch (Exception e) {
@@ -56,3 +69,48 @@ public class Main {
     }
 }
 
+/*
+try{
+    // Ruta de l'arxiu json llibres
+    String rutaJsonLlibres = "src/main/json/llibres.json";
+    String contingut = new String(Files.readAllBytes(Paths.get(rutaJsonLlibres)));
+
+    // Convertim el contingut a un JSONArray
+    JSONArray llibres = new JSONArray(contingut);
+
+    // Iterem per accedir a cadasucn dels llibres
+    for (int i = 0; i < llibres.length(); i++){
+        // Agafem cada llibre com a Object
+        JSONObject llibre = llibres.getJSONObject(i);
+
+        // Guardem les dades del llibre
+        int id = llibre.getInt("id");
+        String titol = llibre.getString("titol");
+        JSONArray autor = llibre.getJSONArray("autor");
+
+        // Mostrem les dades del llibre
+        if (i == 0){
+            System.out.println("-".repeat(38) + "   " + "LLISTAT DE LLIBRES" + "    " + "-".repeat(38));
+            System.out.printf("%-5s | %-45s | %-35s", "ID", "TITOL", "AUTOR");
+            System.out.println();
+            System.out.println("-".repeat(100));
+        }
+        System.out.printf("%-5d | %-45s | ", id, titol);
+        for (int j = 0; j < autor.length(); j++){
+            if (j != 0){
+                System.out.print(" / " + autor.getString(j));
+            }else{
+                System.out.print(autor.getString(j));
+            }
+        }
+        System.out.println("");
+        if (i == (llibres.length() - 1)){
+            System.out.println();
+        } else {
+            System.out.println("-".repeat(100));
+        }
+    }
+} catch (Exception e) {
+    e.printStackTrace();
+}
+*/
