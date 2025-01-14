448af1cc697696512d906dfa0ef27291876b2aac
Javier-lopez-mestre <Xalomes276@gmail.com>
2025-01-14 19:23:43 +0100

    Llista de llibres per paraula en el titol

--- src/main/java/com/biblioteca/Main.java
+++ src/main/java/com/biblioteca/Main.java
@@ -12,20 +12,19 @@ import org.json.JSONObject;
 public class Main {
     public static void main(String[] args) {
         try{
-            
             // Ruta de l'arxiu json llibres
             String rutaJsonLlibres = "src/main/json/llibres.json";
             String contingut = new String(Files.readAllBytes(Paths.get(rutaJsonLlibres)));
             
-            // Demanem la paraula autor
+            // Demanem la paraula que volem buscar en el titol
             Scanner scanner = new Scanner(System.in);
-            System.out.print("Introduce el nombre del autor: ");
-            String autorIntroduit = scanner.nextLine();
-            
-            // Convertim el contingut a un JSONArray
+            System.out.print("Introdueix la paraula que vols buscar en els titols: ");
+            String paraulaTitol = scanner.nextLine();
+
+            // Convertim el contingut a JSONArray
             JSONArray llibres = new JSONArray(contingut);
 
-            // Iterem per accedir a cadasucn dels llibres
+            //Iterem per accedir a cadascun dels llibres
             for (int i = 0; i < llibres.length(); i++){
                 // Agafem cada llibre com a Object
                 JSONObject llibre = llibres.getJSONObject(i);
@@ -34,42 +33,114 @@ public class Main {
                 int id = llibre.getInt("id");
                 String titol = llibre.getString("titol");
                 JSONArray autor = llibre.getJSONArray("autor");
-                
+
                 // Mostrem la capçelera de la llista
                 if (i == 0){
-                    System.out.println("-".repeat(33) + "   " + "LLISTAT DE LLIBRES PER AUTOR" + "    " + "-".repeat(33));
+                    System.out.println("-".repeat(33) + "   " + "LLISTAT DE LLIBRES PER TITOL" + "    " + "-".repeat(33));
                     System.out.printf("%-5s | %-45s | %-35s", "ID", "TITOL", "AUTOR");
                     System.out.println();
                     System.out.println("-".repeat(100));
                 }
 
-                // Si l'autor introduit es troba, es mostren les dades d'aquest
-                for (int j = 0; j < autor.length(); j++){
-                    if (autorIntroduit.equals(autor.getString(j))){
-                        System.out.printf("%-5s | %-45s | ", id, titol);
-                        for (int k = 0; k < autor.length(); k++){
-                            if (k != 0){
-                                System.out.print(" / " + autor.getString(j));
-                            }else{
-                                System.out.print(autor.getString(j));
-                            }
-                        }
-                        System.out.println("");
-                        if (i == (llibres.length() - 1)){
-                            System.out.println();
+                // Si trobem la paraula introduida en algun titol, mostrem el id, titol i autor/autors del llibre
+                if ((titol.toLowerCase()).contains((paraulaTitol.toLowerCase()))){
+                    System.out.printf("%-5d | %-45s | ", id, titol);
+                    for (int j = 0; j < autor.length(); j++){
+                        if (j != 0){
+                            System.out.print(" / " + autor.getString(j));
                         } else {
-                            System.out.println("-".repeat(100));
+                            System.out.print(autor.getString(j));
                         }
+                    }
+                    // Deixem el salt de linea entre llibre i llibre
+                    System.out.println();
+                    if (i == (llibres.length() - 1)){
+                        System.out.println();
+                    } else {
+                        System.out.println("-".repeat(100));
                     }
                 }
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
 
+
+
+
+
+
 /*
+Llistat de llibres per autor
+try{
+    // Ruta de l'arxiu json llibres
+    String rutaJsonLlibres = "src/main/json/llibres.json";
+    String contingut = new String(Files.readAllBytes(Paths.get(rutaJsonLlibres)));
+    
+    // Demanem la paraula autor
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Introduce el nombre del autor: ");
+    String autorIntroduit = scanner.nextLine();
+    
+    // Convertim el contingut a un JSONArray
+    JSONArray llibres = new JSONArray(contingut);
+
+    // Iterem per accedir a cadasucn dels llibres
+    for (int i = 0; i < llibres.length(); i++){
+        // Agafem cada llibre com a Object
+        JSONObject llibre = llibres.getJSONObject(i);
+
+        // Guardem les dades del llibre
+        int id = llibre.getInt("id");
+        String titol = llibre.getString("titol");
+        JSONArray autor = llibre.getJSONArray("autor");
+        
+        // Mostrem la capçelera de la llista
+        if (i == 0){
+            System.out.println("-".repeat(33) + "   " + "LLISTAT DE LLIBRES PER AUTOR" + "    " + "-".repeat(33));
+            System.out.printf("%-5s | %-45s | %-35s", "ID", "TITOL", "AUTOR");
+            System.out.println();
+            System.out.println("-".repeat(100));
+        }
+
+        // Si l'autor introduit es troba, es mostren les dades d'aquest
+        for (int j = 0; j < autor.length(); j++){
+            if (autorIntroduit.equals(autor.getString(j))){
+                System.out.printf("%-5s | %-45s | ", id, titol);
+                for (int k = 0; k < autor.length(); k++){
+                    if (k != 0){
+                        System.out.print(" / " + autor.getString(j));
+                    }else{
+                        System.out.print(autor.getString(j));
+                    }
+                }
+                System.out.println("");
+                if (i == (llibres.length() - 1)){
+                    System.out.println();
+                } else {
+                    System.out.println("-".repeat(100));
+                }
+            }
+        }
+    }
+} catch (Exception e) {
+    e.printStackTrace();
+}
+*/
+
+
+
+
+
+
+
+
+
+/*
+Llistat de llibres
 try{
     // Ruta de l'arxiu json llibres
     String rutaJsonLlibres = "src/main/json/llibres.json";
